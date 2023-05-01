74ca235519341e5895628c1168f96f6c1b8f91a6
Keve Lajsz <kevelajsz@icloud.com>
2023-05-01 16:30:09 +0200

    add test endpoints for all roles

--- src/main/java/hu/bathorydse/utrserver/controllers/TestController.java
+++ src/main/java/hu/bathorydse/utrserver/controllers/TestController.java
@@ -10,26 +10,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
-	@GetMapping("/all")
-	public String allAccess() {
-		return "Public Content.";
-	}
-	
-	@GetMapping("/user")
-	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-	public String userAccess() {
-		return "User Content.";
-	}
 
-	@GetMapping("/mod")
-	@PreAuthorize("hasRole('MODERATOR')")
-	public String moderatorAccess() {
-		return "Moderator Board.";
-	}
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public access.";
+    }
 
-	@GetMapping("/admin")
-	@PreAuthorize("hasRole('ADMIN')")
-	public String adminAccess() {
-		return "Admin Board.";
-	}
+    @GetMapping("/speaker")
+    @PreAuthorize("hasRole('SPEAKER')")
+    public String speakerAccess() {
+        return "Speaker access.";
+    }
+
+    @GetMapping("/allitobiro")
+    @PreAuthorize("hasRole('ALLITOBIRO')")
+    public String allitobiroAccess() {
+        return "Állítóbíró access.";
+    }
+
+    @GetMapping("/idorogzito")
+    @PreAuthorize("hasRole('IDOROGZITO')")
+    public String idorogzitoAccess() {
+        return "Időrögzítő access.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin access.";
+    }
 }
