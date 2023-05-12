d5e86591462e514e5430c7c1fb28c004c0303aec
Keve Lajsz <kevelajsz@icloud.com>
2023-05-12 15:12:17 +0200

    add application logo SVG asset

---
+++ src/components/AppLogo.tsx
@@ -0,0 +1,12 @@
+import {ReactComponent as Logo} from "../assets/utr_logo.svg";
+
+export function AppLogo(props: {
+    scale?: number
+    className?: string
+}) {
+    return (
+        <Logo style={{
+            transform: `scale(${props.scale}%)`
+        }} className={props.className}/>
+    );
+}
