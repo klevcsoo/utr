c0c033a772a113d52423790d1e5c49939b19d1ff
Keve Lajsz <kevelajsz@icloud.com>
2023-05-12 14:34:09 +0200

    resolve #1 add TextInput styling

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -6,6 +6,8 @@ export function TextInput(props: {
 }) {
     return (
         <input type={props.password ? "password" : "text"} value={props.value}
+               className="max-w-sm h-8 px-2 border-2 rounded-md
+               border-slate-200 bg-slate-100 focus:outline-none focus:border-sky-400"
                onChange={event => {
                    props.onValue(event.currentTarget.value);
                }}
