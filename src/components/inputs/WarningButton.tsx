bf68772f5f607e5bb42295eb6f784489f2148c0d
Keve Lajsz <kevelajsz@icloud.com>
2023-05-15 23:43:00 +0200

    add WarningButton

---
+++ src/components/inputs/WarningButton.tsx
@@ -0,0 +1,14 @@
+export function WarningButton(props: {
+    text: string
+    onClick(): void
+    disabled?: boolean
+}) {
+    return (
+        <button type="button" disabled={props.disabled} onClick={props.onClick}
+                className="max-w-xs min-w-max w-full px-4 py-1
+                rounded-md bg-red-400 text-white
+                hover:bg-sky-600 transition-all">
+            {props.text}
+        </button>
+    );
+}
