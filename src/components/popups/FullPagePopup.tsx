a312d51d7889e49008a690e3d8eba899b4c6f991
Keve Lajsz <kevelajsz@icloud.com>
2023-05-16 18:37:22 +0200

    add className prop to FullPagePopup

--- src/components/popups/FullPagePopup.tsx
+++ src/components/popups/FullPagePopup.tsx
@@ -2,13 +2,14 @@ import {ReactNode} from "react";
 
 export function FullPagePopup(props: {
     children: ReactNode
+    className?: string
     onClickOutside?(): void
 }) {
     return (
         <div className="fixed inset-0 backdrop-blur-xl backdrop-saturate-150
 		grid place-content-center">
-            <div className="max-w-md min-w-max bg-white rounded-lg
-            overflow-hidden shadow-xl p-8">
+            <div className={"max-w-md min-w-max bg-white rounded-lg " +
+                "overflow-hidden shadow-xl " + (props.className ?? "")}>
                 {props.children}
             </div>
         </div>
