fefc262cb80379cd456b6a3bb0046c0d718bd910
Keve Lajsz <kevelajsz@icloud.com>
2023-05-16 20:00:10 +0200

    add AdminLayout title and navigation

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,16 +1,34 @@
 import {NavbarLayout} from "./NavbarLayout";
-import {Outlet} from "react-router-dom";
-import React from "react";
+import {Link, Outlet} from "react-router-dom";
+import React, {createContext, useState} from "react";
+import {RawMaterialIcon} from "../components/icons/RawMaterialIcon";
+
+export const AdminLayoutTitleContext = createContext<(title: string) => void>(
+    () => {
+    }
+);
 
 export function AdminLayout() {
-	return (
-		<div className="grid grid-cols-[20rem_auto]
+    const [title, setTitle] = useState("Áttekintés");
+
+    return (
+        <div className="grid grid-cols-[20rem_auto]
                         w-screen h-screen overflow-hidden">
-			<NavbarLayout/>
-			<div className="w-full h-full
+            <NavbarLayout/>
+            <div className="w-full h-full p-8 flex flex-col items-start gap-4
                             overflow-x-hidden overflow-y-scroll">
-				<Outlet/>
-			</div>
-		</div>
-	);
+                <Link to=".." relative="path"
+                      className="flex flex-row gap-1 group
+                      items-center justify-start">
+                    <RawMaterialIcon name="arrow_back_ios" className="scale-150"/>
+                    <h1 className="group-hover:underline">
+                        <b>{title}</b>
+                    </h1>
+                </Link>
+                <AdminLayoutTitleContext.Provider value={setTitle}>
+                    <Outlet/>
+                </AdminLayoutTitleContext.Provider>
+            </div>
+        </div>
+    );
 }
