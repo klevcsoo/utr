bee041eeb3028dcfee33130b9146ef6f58e55dcf
Keve Lajsz <kevelajsz@icloud.com>
2023-05-16 19:59:36 +0200

    fix TitleIcon icon font colour

--- src/components/icons/TitleIcon.tsx
+++ src/components/icons/TitleIcon.tsx
@@ -5,8 +5,8 @@ export function TitleIcon(props: {
 }) {
     return (
         <div className="w-16 h-16 grid place-content-center
-        bg-blue-200 rounded-full">
-            <RawMaterialIcon name={props.name} className="text-blue-500 scale-125"/>
+        bg-blue-200 text-blue-500 rounded-full">
+            <RawMaterialIcon name={props.name} className="scale-125"/>
         </div>
     );
 }
