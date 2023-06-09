8f3fedecc81cc59799a40981fab67c704ce13dbf
Keve Lajsz <kevelajsz@gmail.com>
2023-06-09 16:29:36 +0200

    resolve #17 React Router base path

--- utr-client/src/index.tsx
+++ utr-client/src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <AuthProvider>
-        <BrowserRouter basename="/client">
+        <BrowserRouter>
             <App/>
         </BrowserRouter>
     </AuthProvider>
