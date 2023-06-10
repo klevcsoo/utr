0dd7bcbcca67c1db1fabb396512283b903ae5e1d
Keve Lajsz <kevelajsz@gmail.com>
2023-06-10 14:26:39 +0200

    add uszoversenyek API support

---
+++ utr-client/src/api/versenyek.ts
@@ -0,0 +1,36 @@
+import {Uszoverseny} from "../types/Uszoverseny";
+import {apiRequest, createAllStringObject} from "../utils";
+import {UserDetails} from "../types/UserDetails";
+import {MessageResponse} from "../types/MessageResponse";
+
+export async function getAllUszoversenyekList(user: UserDetails): Promise<Uszoverseny[]> {
+    const data = await apiRequest<Uszoverseny[]>(user, "/uszoversenyek/", "GET");
+    for (const verseny of data) {
+        verseny.datum = new Date(verseny.datum);
+    }
+    return data;
+}
+
+export async function getUszoverseny(user: UserDetails, id: number): Promise<Uszoverseny> {
+    return apiRequest<Uszoverseny>(user, `/uszoversenyek/${id}`, "GET");
+}
+
+export async function createUszoverseny(
+    user: UserDetails, data: Omit<Uszoverseny, "id">
+): Promise<MessageResponse> {
+    const params = new URLSearchParams(createAllStringObject(data));
+    return apiRequest<MessageResponse>(user, `/csapatok/?${params}`, "PUT");
+}
+
+export async function editUszoverseny(
+    user: UserDetails, id: number, data: Partial<Omit<Uszoverseny, "id">>
+): Promise<MessageResponse> {
+    const params = new URLSearchParams(createAllStringObject(data));
+    return apiRequest(user, `/uszoversenyek/${id}?${params}`, "PATCH");
+}
+
+export async function deleteUszoverseny(
+    user: UserDetails, id: number
+): Promise<MessageResponse> {
+    return apiRequest(user, `/uszoversenyek/${id}`, "DELETE");
+}
