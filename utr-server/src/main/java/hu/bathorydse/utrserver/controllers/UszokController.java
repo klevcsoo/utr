23a7952255ad39a6a1b164f9c7000e74b77d392c
Keve Lajsz <kevelajsz@gmail.com>
2023-06-10 11:00:08 +0200

    resolve #15 CORS policy

--- utr-server/src/main/java/hu/bathorydse/utrserver/controllers/UszokController.java
+++ utr-server/src/main/java/hu/bathorydse/utrserver/controllers/UszokController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:3000", "https://utr.hu"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api/uszok")
 @PreAuthorize("hasRole('ADMIN')")
--- utr-server/src/main/java/hu/bathorydse/utrserver/controllers/VersenyszamokController.java
+++ utr-server/src/main/java/hu/bathorydse/utrserver/controllers/VersenyszamokController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:3000", "https://utr.hu"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api/versenyszamok")
 @PreAuthorize("hasRole('ADMIN')")
