54ab9cffbca35e39516ba6be2cd1b66088f18c71
Keve Lajsz <kevelajsz@gmail.com>
2023-06-20 04:33:27 +0200

    fix Dropdown component functionality issues

--- utr-client/src/components/inputs/Dropdown.tsx
+++ utr-client/src/components/inputs/Dropdown.tsx
@@ -1,11 +1,12 @@
 import {useMemo} from "react";
 
-export function Dropdown(props: {
-    options: string[] | { [key: string]: string }
-    onSelect(index: number): void
+export function Dropdown<T extends string>(props: {
+    options: T[] | readonly T[] | { [key in T]: string }
+    selected: T
+    onSelect(value: T): void
 }) {
     const resolvedOptions = useMemo<[string, string][]>(() => {
-        if (!!props.options["join"]) {
+        if (props.options instanceof Array) {
             return (props.options as string[]).map(value => [value, value]);
         } else {
             return Object.keys(props.options).map(key => {
@@ -18,7 +19,10 @@ export function Dropdown(props: {
         <select id={`dropdown-${(new Date()).getTime()}${Math.random() * 100}`}
                 className="max-w-xs w-full h-8 px-2 border-2 rounded-md
                 border-slate-200 bg-slate-100 focus:outline-none
-                focus:border-blue-400">
+                focus:border-blue-400" value={props.selected}
+                onChange={event => {
+                    props.onSelect(event.currentTarget.value as T);
+                }}>
             {resolvedOptions.map(([value, displayName], index) => (
                 <option key={index} value={value}>{displayName}</option>
             ))}
