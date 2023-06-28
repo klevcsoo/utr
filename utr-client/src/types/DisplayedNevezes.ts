d6233525542eed64646d74b9da512923174c0e17
Keve Lajsz <kevelajsz@gmail.com>
2023-06-28 12:22:46 +0200

    add DisplayedNevezes type

---
+++ utr-client/src/types/DisplayedNevezes.ts
@@ -0,0 +1,10 @@
+import {Identifiable} from "./Identifiable";
+
+export type DisplayedNevezes = Identifiable<{
+    uszoNev: string
+    uszoSzuletesiEv: number
+    csapatNev: string
+    nevezesiIdo: string
+    idoeredmeny: string
+    megjelent: boolean
+}>
