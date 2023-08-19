eaf9a3c7e4e56be0242899d0f83a95bbaf1cfb48
Keve Lajsz <kevelajsz@gmail.com>
2023-08-19 17:04:50 +0200

    refs #71 remove unnecessary team from swimmer model

--- utr-api-v2/models/swimmer.model.go
+++ utr-api-v2/models/swimmer.model.go
@@ -6,8 +6,7 @@ type Swimmer struct {
 	utils.BaseModel
 	Name        string `json:"name" gorm:"type:text;not null;index"`
 	TeamID      int    `json:"-" gorm:"not null"`
-	Team        *Team  `json:"team"`
-	SexID       int    `json:"-"`
+	SexID       string `json:"-"`
 	Sex         *Sex   `json:"sex"`
 	YearOfBirth uint   `json:"yearOfBirth" gorm:"type:integer;not null"`
 }
