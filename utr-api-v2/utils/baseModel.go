d412d790ae25467d39ad08390cb9c0d61f6cb377
Keve Lajsz <kevelajsz@gmail.com>
2023-08-19 17:04:31 +0200

    refs #71 add json keys to base model

--- utr-api-v2/utils/baseModel.go
+++ utr-api-v2/utils/baseModel.go
@@ -5,7 +5,7 @@ import (
 )
 
 type BaseModel struct {
-	ID        uint       `gorm:"primaryKey"`
-	CreatedAt *time.Time `gorm:"not null;default:now()"`
-	UpdatedAt *time.Time `gorm:"not null;default:now()"`
+	ID        uint       `gorm:"primaryKey" json:"id,omitempty"`
+	CreatedAt *time.Time `gorm:"not null;default:now()" json:"createdAt,omitempty"`
+	UpdatedAt *time.Time `gorm:"not null;default:now()" json:"updatedAt,omitempty"`
 }
