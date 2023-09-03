9e0cbfd49e3b82f352001efa87fad9ffe83315ce
Keve Lajsz <kevelajsz@gmail.com>
2023-09-03 17:32:27 +0200

    refs #71 change entry model

--- utr-api-v2/models/entry.model.go
+++ utr-api-v2/models/entry.model.go
@@ -1,17 +1,27 @@
 package models
 
 import (
-	"github.com/jackc/pgx/v5/pgtype"
+	"time"
+	"utr-api-v2/schemas"
 	"utr-api-v2/utils"
 )
 
 type Entry struct {
 	utils.BaseModel
-	SwimmerID  int              `json:"-" gorm:"uniqueIndex:entry_swimmer_race_index"`
-	Swimmer    *Swimmer         `json:"swimmer"`
-	RaceID     int              `json:"-" gorm:"not null;uniqueIndex:entry_swimmer_race_index"`
-	Race       *Race            `json:"race"`
-	Present    bool             `json:"present" gorm:"type:boolean;default:true;not null"`
-	EntryTime  *pgtype.Interval `json:"entryTime" gorm:"type:interval"`
-	TimeResult *pgtype.Interval `json:"timeResult" gorm:"type:interval;index"`
+	SwimmerID  int           `json:"-" gorm:"uniqueIndex:entry_swimmer_race_index"`
+	Swimmer    *Swimmer      `json:"swimmer"`
+	RaceID     int           `json:"-" gorm:"not null;uniqueIndex:entry_swimmer_race_index"`
+	Race       *Race         `json:"race"`
+	Present    bool          `json:"present" gorm:"type:boolean;default:true;not null"`
+	EntryTime  time.Duration `json:"entryTime" gorm:"type:bigint"`
+	TimeResult time.Duration `json:"timeResult" gorm:"type:bigint;index"`
+}
+
+func NewEntry(raceID int, request schemas.CreateEntryRequest) Entry {
+	return Entry{
+		RaceID:    raceID,
+		SwimmerID: request.Swimmer,
+		EntryTime: time.Duration(request.EntryTime),
+		Present:   true,
+	}
 }
