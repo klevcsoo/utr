968f73bc7f2e0851e22b8213cd3728b90cdb4d34
Keve Lajsz <kevelajsz@gmail.com>
2023-09-03 17:31:51 +0200

    refs #71 add entry schemas

---
+++ utr-api-v2/schemas/entry.schemas.go
@@ -0,0 +1,12 @@
+package schemas
+
+type CreateEntryRequest struct {
+	Swimmer   int `json:"swimmer,omitempty" validate:"required"`
+	EntryTime int `json:"entryTime,omitempty"`
+}
+
+type EditEntryRequest struct {
+	EntryTime  int `json:"entryTime,omitempty"`
+	TimeResult int `json:"timeResult,omitempty"`
+	Present    int `json:"present,omitempty"`
+}
